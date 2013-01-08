e1ee241368dd51a2ad6f757e596e4a719df34415
richard-to <gohn76@gmail.com>
2013-01-08 03:08:31 -0900

    Add default endo view controller. Need to unit test

--- public/js/lib/endo.js
+++ public/js/lib/endo.js
@@ -4,6 +4,89 @@ define(['backbone'], function(Backbone) {
 
 	// Base View Controller	
 	Endo.ViewController = Backbone.View.extend({
+	// Constants
+	// ---------
+	Endo.SEGUE_STYLE_PUSH = 'push';
+	Endo.SEGUE_STYLE_MODAL = 'modal';
+
+
+	// Endo.ViewController
+	// -------------------
+	
+	// The view that all other Endo views inherit from.
+	//
+	// *Based on iOS ViewController.*
+
+	// Create an Endo.ViewController.
+	Endo.ViewController = function(options) {
+		var propList = ['title', 'segues'];
+		options = options || {};
+		this._configureProps(options || {}, propList);
+		this.template = options['template'] || null;
+		Backbone.View.apply(this, [options]);		
+	};
+
+	_.extend(Endo.ViewController.prototype, Backbone.View.prototype, {
+
+		// Title of view.
+		//
+		// May be used for navigation back button text.
+		// Another use may be the title bar in browser.
+		title: null,
+
+		// If this is a nested view, we can get the parent view controller.
+		parentViewController: null,
+
+		// If this is a navigation view controller, we can get a reference.
+		navigationController: null,
+
+		// A dictionary of views we can transition to.
+		// Currently only works with Navigation Controller.
+		//
+		// Examples:
+		//
+		// segues: {
+		//     identifier: {
+		//         style: Endo.SEGUE_STYLE_PUSH,
+		//         destinationViewController: controller	
+		//	   }	
+		// }
+		//
+	    // segues: {
+		//     identifier: controller
+		// }
+		segues: {},
+		
+		// Performs the transition to the next view if there is a
+		// navigation controller.
+		performSegue: function(identifier) {
+			var segue = this.segues[identifier];
+
+			if (this.navigationController && segue) {
+				if (_.isObject(segue) && segue.destinationViewController) {
+					segue.style = segue.style || Endo.SEGUE_STYLE_PUSH;
+				} else {
+					segue = {
+						style: Endo.SEGUE_STYLE_PUSH,
+						destinationViewController: segue
+					};
+				}
+				this.prepareSegue(segue);
+				this.navigationController.pushViewController(segue);
+			}
+		},
+
+		// Override this method to setup destination view controller
+		// before it gets pushed onto Navigation controller.
+		prepareSegue: function(segue) {
+
+		},
+
+		// Set custom view options to be merged with property list
+		_configureProps: function(options, propList) {
+      		if (this.options) options = _.extend({}, _.result(this, 'options'), options);
+      		_.extend(this, _.pick(options, propList));			
+		}
 	});
 
 	// Navigation View Controller
