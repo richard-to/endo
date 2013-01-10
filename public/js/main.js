283cd748f2b74d9eff2b43b2d2d7da0f1f7caced
richard-to <gohn76@gmail.com>
2013-01-10 01:20:57 -0900

    Add some test code for manual acceptance for now.

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,20 @@
 require(['config'], function() {
 require(['Endo'], function(Endo) {
-    
+ 
+     var TestViewControllerTwoLinks = Endo.ViewController.extend({
+        el: '<p><a class="test">Test Link</a> <a class="test2">Test Link2</a></p>',
+        events: {
+            'click .test': 'nextView',
+            'click .test2': 'nextView2'
+        },
+        nextView: function(events) {
+            this.performSegue('segueTest');    
+        },
+        nextView2: function(events) {
+            this.performSegue('segueTest2');    
+        }        
+    });
+   
     var TestViewController = Endo.ViewController.extend({
         el: '<p><a class="test">Test Link</a></p>',
         events: {
@@ -23,10 +37,18 @@ require(['Endo'], function(Endo) {
         }
     });
 
-
-    var root = new TestViewController({
+    var nextAlt = new TestViewController({
+        el: '<p><a class="test">Test Link Next Alt</a></p>',
+        title: 'NEXT ALT',
         segues: {
-            segueTest: next
+            segueTest: nextNext
+        }
+    });
+
+    var root = new TestViewControllerTwoLinks({
+        segues: {
+            segueTest: next,
+            segueTest2: nextAlt
         }
     });
 
