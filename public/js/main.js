30de9daf3c4c47f602e477c7a485d064af6f1727
richard-to <gohn76@gmail.com>
2013-01-10 01:15:35 -0900

    Add some test code to view navigation in action.

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,39 @@
 require(['config'], function() {
+require(['Endo'], function(Endo) {
+    
+    var TestViewController = Endo.ViewController.extend({
+        el: '<p><a class="test">Test Link</a></p>',
+        events: {
+            'click .test': 'nextView'
+        },
+        nextView: function(events) {
+            this.performSegue('segueTest');    
+        }
+    });
 
+    var nextNext = new Endo.ViewController({
+        el: "<div>NEXT NEXT PAGE</div>"
+    });
+
+    var next = new TestViewController({
+        el: '<p><a class="test">Test Link Next</a></p>',
+        title: 'NEXT',
+        segues: {
+            segueTest: nextNext
+        }
+    });
+
+
+    var root = new TestViewController({
+        segues: {
+            segueTest: next
+        }
+    });
+
+    var nc = new Endo.NavigationViewController({
+        el: document.body,
+        rootViewController: root
+    });
+    nc.render();
+});
 });
